2391356eb1a7b3feb7b35affd2f7d92a5435ec7d
aketcham10 <186063166+aketcham10@users.noreply.github.com>
2025-10-06 17:10:43 -0400

    Added Frontend files

---
+++ script.js
@@ -0,0 +1,10 @@
+addEventListener("DOMContentLoaded", async function() {
+    const response = await fetch("http://localhost:3000/api/songs");
+    const songs = await response.json();
+    
+    let html = "";
+    for (const song of songs) {
+        html += `<li>${song.title}</li>`;
+    }
+    document.querySelector("#addedsong").innerHTML = html;
+})
